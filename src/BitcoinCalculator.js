18306eb47fce79e90d92712f5fe58aa3d20402cc
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-08 11:04:59 +0100

    first commit

---
+++ src/BitcoinCalculator.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+// Utilizamos useState para declarar variables de estado y sus funciones de actualización. Aquí manejamos la inversión inicial, el precio actual de Bitcoin, la cantidad de Bitcoin comprada y la ganancia calculada.
+const BitcoinCalculator = () => {
+  const [initialInvestment, setInitialInvestment] = useState('');
+  const [currentPrice, setCurrentPrice] = useState('');
+  const [btcAmount, setBtcAmount] = useState('');
+  const [profit, setProfit] = useState(null);
+// Valor de bitcoin 
+  const handleCalculate = () => {
+    const investment = parseFloat(initialInvestment);
+    const price = parseFloat(currentPrice);
+    const amount = parseFloat(btcAmount);
+    const calculatedProfit = (price * amount) - investment;
+    setProfit(calculatedProfit);
+  };
+
+  return (
+    <div>
+      <h1>Calculadora de Ganancia en Bitcoin</h1>
+      <div>
+        <label>
+          Inversión Inicial (USD):
+          <input
+            type="number"
+            value={initialInvestment}
+            onChange={(e) => setInitialInvestment(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Precio Actual de Bitcoin (USD):
+          <input
+            type="number"
+            value={currentPrice}
+            onChange={(e) => setCurrentPrice(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Cantidad de Bitcoin Comprado:
+          <input
+            type="number"
+            value={btcAmount}
+            onChange={(e) => setBtcAmount(e.target.value)}
+          />
+        </label>
+      </div>
+      <button onClick={handleCalculate}>Calcular Ganancia</button>
+      {profit !== null && (
+        <div>
+          <h2>Ganancia: ${profit.toFixed(2)}</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BitcoinCalculator;
+
+
