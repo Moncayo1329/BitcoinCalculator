da19c17808067bc823b5967fbccb140100876220
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-10 18:26:00 +0100

    adding decimals and button

--- src/BitcoinCalculator.js
+++ src/BitcoinCalculator.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 // Utilizamos useState para declarar variables de estado y sus funciones de actualización. Aquí manejamos la inversión inicial, el precio actual de Bitcoin, la cantidad de Bitcoin comprada y la ganancia calculada.
+
 const BitcoinCalculator = () => {
   const [initialInvestment, setInitialInvestment] = useState('');
   const [currentPrice, setCurrentPrice] = useState('');
   const [btcAmount, setBtcAmount] = useState('');
   const [profit, setProfit] = useState(null);
-// Valor de bitcoin 
+// Valor de bitcoin
   const handleCalculate = () => {
     const investment = parseFloat(initialInvestment);
     const price = parseFloat(currentPrice);
     const amount = parseFloat(btcAmount);
     const calculatedProfit = (price * amount) - investment;
     setProfit(calculatedProfit);
+  };
+ // Nueva función para reiniciar los valores
+  const handleReset = () => {
+    setInitialInvestment('');
+    setCurrentPrice('');
+    setBtcAmount('');
+    setProfit(null);
+  };
+
+  // Función para formatear números
+  const formatNumber = (num) => {
+    return num.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
   };
 
   return (
@@ -48,10 +61,11 @@ const BitcoinCalculator = () => {
         </label>
       </div>
       <button type="button" className='btn-outline-secondary' onClick={handleCalculate}>Calcular Ganancia</button>
+      <button type="button" className='btn-outline-secondary' onClick={handleReset}>Reiniciar</button>
    
       {profit !== null && (
         <div>
-          <h2>Ganancia: ${profit.toFixed(2)}</h2>
+          <h2>Ganancia: ${formatNumber(profit)}</h2>
         </div>
       )}
     </div>
