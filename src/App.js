13d119d8cd8d3e923864674cd139df1a93d91c9d
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-07-13 00:28:47 +0100

    finally finish media scren

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
 import BitcoinCalculator from './BitcoinCalculator';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
       <BitcoinCalculator/>
+      <Footer />
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Footer(){
+
+return(
+<div>
+
+<p>Made by Michael Moncayo</p>
+
+</div>
+
+)
+
+
+}
+
+
+
+
+export default Footer
